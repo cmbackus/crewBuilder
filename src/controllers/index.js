024c4863e926975a61326c0796fddd2ee34643fa
Charlie Backus <cbackus123@gmail.com>
2015-04-23 13:14:39 -0400

    css, wording

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,3 @@
 module.exports.Account = require('./Account.js');
-module.exports.Domo = require('./Domo.js');
 module.exports.Vol = require('./Vol.js');
 module.exports.Crew = require('./Crew.js');
